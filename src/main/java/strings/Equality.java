0c0eb5fccad1a0c3291d63a130111ec006928d3d
Inder Bagga <er.inderbagga@gmail.com>
2021-06-03 21:59:20 +0530

    Equality in Java Strings.

---
+++ src/main/java/strings/Equality.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class Equality {
+
+    public static void main(String[] args){
+
+        String str="ABC";
+        String object=new String("ABC");
+
+        System.out.println(str==object);//false; because memory address is different.
+        System.out.println(str.equals(object));//true' because contents are same.
+    }
+}
