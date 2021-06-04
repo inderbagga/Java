f41cf0f67a6cadd0955a72805667754c07fd0a9d
Inder Bagga <er.inderbagga@gmail.com>
2021-06-04 15:51:25 +0530

    Added Identity HashCodes to analyse reference equality.

--- src/main/java/strings/Equality.java
+++ src/main/java/strings/Equality.java
@@ -7,6 +7,9 @@ public class Equality {
         String str="ABC";
         String object=new String("ABC");
 
+        System.out.println(System.identityHashCode( str ));
+        System.out.println(System.identityHashCode( object ));
+
         System.out.println(str==object);//false; because memory address is different.
         System.out.println(str.equals(object));//true' because contents are same.
     }
