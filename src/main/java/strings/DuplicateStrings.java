8af62910d1baca554e81c67a2c6c28c43b23d06c
Inder Bagga <er.inderbagga@gmail.com>
2021-06-04 16:48:45 +0530

    Updated program logic.

--- src/main/java/strings/DuplicateStrings.java
+++ src/main/java/strings/DuplicateStrings.java
@@ -7,33 +7,37 @@ public class DuplicateStrings {
 
     public static void main(String[] arg) {
 
-        String word = "Great Responsibility!";
+        String word = "Shameless!";
 
-        System.out.println("Duplicate characters in: " + word);
-
+        int comparator=0;
         int occurence[] = new int[word.length()];
         List duplicates = new ArrayList<Character>();
 
-        for (int i = 0; i < word.length(); i++) {
+        System.out.println("Duplicate characters in: " + word);
+
+        for (int i = 0; i < word.length(); i++,comparator++) {
 
             //every letter is present once by default
             occurence[i] = 1;
 
-            for (int j = 1; j < word.length() && i != j; j++) {
+            for (int j = i+1; j < word.length() && i != j; j++,comparator++) {
 
                 if (word.charAt(i) == word.charAt(j)) {
-                    //increase duplicate occurence
                     occurence[i] = occurence[i] + 1;
                 }
             }
         }
 
-        for (int i = occurence.length - 1; i >= 0; i--) {
-            if (occurence[i] > 1 && !duplicates.contains(word.charAt(i))) {
-                duplicates.add(word.charAt(i));
-                System.out.println("The duplicate character " + word.charAt(i) + " appeared " + occurence[i] + " times.");
+        System.out.println("Iterations performed during stage1: "+ comparator);
+
+        for (int k =0;k< occurence.length - 1;  k++,comparator++) {
+            if (occurence[k] > 1 && !duplicates.contains(word.charAt(k))) {
+                duplicates.add(word.charAt(k));
+                System.out.println("The duplicate character " + word.charAt(k) + " appeared " + occurence[k] + " times.");
             }
         }
+
+            System.out.println("Total iterations executed after stage2: "+ comparator);
 
         duplicates.clear();
         occurence=null;
