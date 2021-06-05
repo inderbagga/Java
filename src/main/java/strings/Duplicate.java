11c6016f19a16be25ddddbea9ffd2b82c5b61067
Inder Bagga <er.inderbagga@gmail.com>
2021-06-05 10:02:31 +0530

    Updated duplicate Strings execution.

--- src/main/java/strings/Duplicate.java
+++ src/main/java/strings/Duplicate.java
@@ -3,17 +3,24 @@ package strings;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DuplicateStrings {
+public class Duplicate {
 
     public static void main(String[] arg) {
 
         String word = "Shameless!";
 
+        duplicateLetterCount(word);
+        duplicateLetters(word);
+    }
+
+
+    public static void duplicateLetterCount(String word) {
+
         int comparator=0;
         int occurence[] = new int[word.length()];
         List duplicates = new ArrayList<Character>();
 
-        System.out.println("Duplicate characters in: " + word);
+        System.out.println("==== Duplicate characters with count in: " + word+" ====");
 
         for (int i = 0; i < word.length(); i++,comparator++) {
 
@@ -41,5 +48,29 @@ public class DuplicateStrings {
 
         duplicates.clear();
         occurence=null;
+    }
+
+    public static void duplicateLetters(String string1) {
+
+        int count;
+
+        //Converts given string into character array
+        char string[] = string1.toCharArray();
+
+        System.out.println("===== Duplicate characters in a given string: =====");
+        //Counts each character present in the string
+        for(int i = 0; i <string.length; i++) {
+            count = 1;
+            for(int j = i+1; j <string.length; j++) {
+                if(string[i] == string[j] && string[i] != ' ') {
+                    count++;
+                    //Set string[j] to 0 to avoid printing visited character
+                    string[j] = '0';
+                }
+            }
+            //A character is considered as duplicate if count is greater than 1
+            if(count > 1 && string[i] != '0')
+                System.out.println(string[i]);
+        }
     }
 }
