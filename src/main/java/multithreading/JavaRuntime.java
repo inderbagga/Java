85917de3cc21857f4c36f48b133f2dc700fdbbfa
Inder Bagga <er.inderbagga@gmail.com>
2021-06-08 13:07:38 +0530

    Shutting down OS using Runtime.

--- src/main/java/multithreading/JavaRuntime.java
+++ src/main/java/multithreading/JavaRuntime.java
@@ -6,16 +6,36 @@ public class JavaRuntime {
 
     public static void main(String[] args){
 
-        openLibreOffice();
+        String operatingSystem=System.getProperty("os.name");
 
+        //openLibreOffice(operatingSystem);//uncomment to open another application.*/
+
+        shutdown(operatingSystem);
     }
 
-    static private void openLibreOffice(){
+    static private void shutdown(String OS){
 
         Runtime runtime=Runtime.getRuntime();
 
         try {
-            runtime.exec("/usr/bin/libreoffice");
+
+            if(OS.equals("Linux")){
+                Process proc =runtime.exec("shutdown -h -t 0");
+                System.exit(0);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static private void openLibreOffice(String OS){
+
+        Runtime runtime=Runtime.getRuntime();
+
+        try {
+            if(OS.equals("Linux"))
+                runtime.exec("/usr/bin/libreoffice");
         } catch (IOException e) {
             e.printStackTrace();
         }
