66aa71ae7432c7324a53f14dfb8ef27d5b2a0ab6
Inder Bagga <er.inderbagga@gmail.com>
2021-06-09 16:04:43 +0530

    Added DeadLock scenario.

---
+++ src/main/java/multithreading/DeadLock.java
@@ -0,0 +1,44 @@
+package multithreading;
+
+public class DeadLock {
+
+    public static void main(String[] args){
+
+        String resource1="India";
+        String resource2="China";
+
+        new Thread("thread1"){
+
+            @Override
+            public void run() {
+
+                synchronized (resource1){
+                    System.out.println(currentThread() +" acquires lock on "+resource1);
+
+                    try { Thread.sleep(2000);} catch (Exception e) {}
+
+                    synchronized (resource2){
+                        System.out.println(currentThread() +" acquires lock on "+resource2);
+                    }
+                }
+            }
+        }.start();
+
+        new Thread("thread2"){
+
+            @Override
+            public void run() {
+
+                synchronized (resource2){
+                    System.out.println(currentThread() +" acquires lock on "+resource2);
+
+                    try { Thread.sleep(2000);} catch (Exception e) {}
+
+                    synchronized (resource1){
+                        System.out.println(currentThread() +" acquires lock on "+resource1);
+                    }
+                }
+            }
+        }.start();
+    }
+}
